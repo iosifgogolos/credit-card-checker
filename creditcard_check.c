7ae64b4acd33b8ce7a029dd21adf59188a662b51
iosifgogolos <82125428+iosifgogolos@users.noreply.github.com>
2022-12-27 19:58:46 +0100

    Update the source code
    Added 3 new cards: Diners Club, Discover, & JCB

--- creditcard_check.c
+++ creditcard_check.c
@@ -5,24 +5,24 @@ int main(void)
 {
     //Step 1: Prompt the user for input
     long card;
-    do
-    {
-        card = get_long("Card number: ");
-    }
-    // Step 2: Identify if the card is valid
-    while (card < 0); // Card number must be greater than 0
-
+    do {
+        {
+            card = get_long("Card number: ");
+        }
+    //Step 2: Identify if the card is valid
+    } while (card < 0); //Card number must be greater than 0
+    
     int card1, card2, card3, card4, card5, card6, card7, card8;
-    card1 = ((card % 100)/10)*2;
+    card1 = ((card % 100)/10*2);
     card2 = ((card % 10000)/1000*2);
     card3 = ((card % 1000000)/100000*2);
     card4 = ((card % 100000000)/10000000*2);
     card5 = ((card % 10000000000)/1000000000*2);
     card6 = ((card % 1000000000000)/100000000000*2);
     card7 = ((card % 100000000000000)/10000000000000*2);
-    card8 = ((card % 10000000000000000)/1000000000000000*2);
-
-    //splitting the digits
+    card7 = ((card % 10000000000000000)/1000000000000000*2);
+    
+    // 2.1 Splitting the digits
     card1 = (card1 % 100/10) + (card1 % 10);
     card2 = (card2 % 100/10) + (card2 % 10);
     card3 = (card3 % 100/10) + (card3 % 10);
@@ -31,11 +31,11 @@ int main(void)
     card6 = (card6 % 100/10) + (card6 % 10);
     card7 = (card7 % 100/10) + (card7 % 10);
     card8 = (card8 % 100/10) + (card8 % 10);
-
-    //Summarizing all those digits together into sum1
+    
+    // 2.2 Summarizing all those digits together into "sum1"
     int sum1 = card1 + card2 + card3 + card4 + card5 + card6 + card7 + card8;
-
-    //Now we need to find the digits that were not multiplied by 2, starting from the last digit
+    
+    // 2.3 Now we need to find the digits that were not multiplied by 2, starting from the last digit
     int card9, card10, card11, card12, card13, card14, card15, card16;
     card9 = (card % 10);
     card10 = ((card % 1000)/100);
@@ -45,63 +45,114 @@ int main(void)
     card14 = ((card % 100000000000)/10000000000);
     card15 = ((card % 10000000000000)/1000000000000);
     card16 = ((card % 1000000000000000)/100000000000000);
-
-    ///Now the sum it all together into sum2
+    
+    // 2.4 Now we sum int all together into sum2
     int sum2 = card9 + card10 + card11 + card12 + card13 + card14 + card15 + card16;
-
-    //put both sums together and form sum3:
+    
+    // 2.5 put both sums together and form sum3:
     int sum3 = sum1 + sum2;
-
-    //check if the last digit in this sum is 0
-
+    
+    // 2.6 Now, check if the last digit in this sum is 0
+    
     int length = 0;
     long visa = card;
     long amex = card;
     long mastercard = card;
-
-    if ((sum3 % 10) != 0) //sum3 % 10 is what we use to find the last digit
+    
+    //Optional: include Diners Club, Discover, and
+        //long dinersclub = card;
+        //long discover = card;
+        //long jcb = card;
+    
+    if ((sum3 % 10) != 0) // 2.7 sum3 % 10 how we determine the last digit
     {
-        printf("INVALID\n"); //Alternative: printf("%s\n", "Invalid!");
-        return 0; //we want to stop the system from running here if it is invalid
+        printf("%s\n", "INVALID!"); // 2.8 Alternative: printf("INVALID!\n");
+        return 0; // 2.9 We want to stop the system from running here if it is invalid
     }
-    // Step 3: If valid, identify the type of credit card used
-    //Now we need to differentiate between VISA, Amex, and Mastercard
-    //We need to know what is the card lenght
+    //Step 3: If valid, identify the type of credit card used
+        //3.1 In the next stzep we need to differentiate between VISA, AMEX, Mastercard, diners club, discover, and jcb
+        // 3.2 we also need to know what the card length is!!
     while (card > 0)
     {
         card = card/10;
         length++;
     }
-    //Identify if the card is a VISA
+    // 3.3 Identify if the card is a VISA
     while (visa >= 10)
     {
         visa /= 10;
     }
     if (visa == 4 && (length == 13 || length == 16))
     {
-        printf("VISA\n"); //alternative: printf("%s\n", "VISA");
-        return 0; //likewise, we don't want the program to run for forever
+        printf("%s\n", "VISA");
+        return 0;
     }
-
-    //Identify if the card is an AMEX
-    while (amex >= 10000000000000) //10^13
+    // 3.4 Identify if the card is a mastercard
+    while (amex >= 10000000000000) // 10^13
     {
         amex /= 10000000000000;
     }
     if (length == 15 && (amex == 34 || amex == 37))
-    {
+        {
         printf("%s\n", "AMEX");
+        return 0;;
+    }
+    else
+    {
+        printf("%s\n", "INVALID");
         return 0;
     }
-
-    //identify is the card is a Mastercard
-    while (mastercard >= 100000000000000) //10^14
+    //3.5 Identify if the card is a diners club
+    while (dinersclub >= 100000000000000) //10 ^14
     {
-        mastercard /= 100000000000000;
+        dinersclub /= 100000000000000;
     }
-    if (length == 16 && (mastercard == 22 ||mastercard == 51 ||mastercard == 52 ||mastercard == 53 ||mastercard == 54 ||mastercard == 55))
+    if (length == 14 && dinersclub == 30)
     {
-        printf("%s\n", "MASTERCARD");
+        printf("%s\n", "Diners Club");
+        return 0;
+    }
+    else
+    {
+        printf("%s\n", "INVALID");
+        return 0;
+    }
+    //3.6 Identify if the card is a Discover card
+    while (discover >= 10000000000000000) // 10^16
+    {
+        discover /= 10000000000000000;
+    }
+    if (length == 16 && discover == 60)
+    {
+        printf("%s\n", "DISCOVER");
+        return 0;;
+    }
+    else
+    {
+        printf("%s\n", "INVALID");
+        return 0;
+    }
+    while (discover >= 10000000000000000) // 10^16
+    {
+        discover /= 10000000000000000;
+    }
+    if (length == 16 && amex == 60)
+    {
+        printf("%s\n", "DISCOVER");
+        return 0;
+    }
+    else
+    {
+        printf("%s\n", "INVALID");
+        return 0;
+    }
+    while (jcb >= 10000000000000000) // 10^16
+    {
+        jcb /= 10000000000000000;
+    }
+    if (length == 16 && jcb == 35)
+    {
+        printf("%s\n", "JCB");
         return 0;
     }
     else
